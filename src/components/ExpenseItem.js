b248424d3fb6ff15b33565a7392e894e81733e9e
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2021-10-22 08:23:35 +0530

    Passing date via Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
-  const expenseDate = new Date(2021, 10, 22);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.6;
+export default function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
