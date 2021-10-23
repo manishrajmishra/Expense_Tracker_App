ffdd2071607e8a762e84a0434c0650818ed1adda
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2021-10-23 09:23:45 +0530

    Organizing Components Files in UI and Expenses

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 export default function App() {
   const expenses = [
