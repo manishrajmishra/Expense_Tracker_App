b443e2e1f28d19e2ac7c1f0c43855ceb96bd3790
Manish Raj <42971673+manishrajmishra@users.noreply.github.com>
2021-10-28 07:49:59 +0530

    Dynamic Data Adding to Array

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,13 @@
 import React from "react";
-import "./ExpenseItem.css";
-import Card from "../UI/Card";
+
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
-      <div>
-        <ExpenseDate date={props.date} />
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
